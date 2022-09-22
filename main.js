b360250f9f611533cf9cd24b1121df1b5b94e4b0
Nasly <naslicita30@hotmail.com>
2022-09-22 17:20:53 -0500

    Termometro actualizado

--- main.js
+++ main.js
@@ -6,10 +6,20 @@ function convertir(){
     document.getElementById('temperatura').style.height=conversion+'%';
     document.getElementById('temperatura').style.backgroundColor='red'
   
-    }
+}
+function reinicio(){
+    document.getElementById('gradosFarenheit').value='';
+    document.getElementById('temperatura').style.height='0px'
+    document.getElementById('celsius').innerText='';
+    
+}
+
 
 let boton=document.getElementById('boton')
 boton.addEventListener('click',convertir)
+
+let reiniciar=document.getElementById('reiniciar')
+reiniciar.addEventListener('click',reinicio)
 
 
 
